b57660af3d0c7dcd6f219a01c34155ad8dc74b77
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-04-30 11:41:40 +0600

    toast added

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -30,7 +30,7 @@ const Register = () => {
 
         await createUserWithEmailAndPassword(email,password);
         await updateProfile({ displayName: name });
-        navigate('/')
+        navigate('/');
        
     }
     const navigateToLogin=event=>{
