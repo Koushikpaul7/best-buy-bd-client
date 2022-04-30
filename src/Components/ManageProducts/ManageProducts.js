6e3a5120e6c8c69195f505179612650efe4d7456
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-04-30 20:12:25 +0600

    delete operation

--- src/Components/ManageProducts/ManageProducts.js
+++ src/Components/ManageProducts/ManageProducts.js
@@ -2,12 +2,21 @@ import React from 'react';
 import UseProducts from '../../Hooks/UseProducts';
 
 const ManageProducts = () => {
-    const[products]=UseProducts();
+    const[products,setProducts]=UseProducts();
 
     const handleDelete=id=>{
         const proceed= window.confirm('are you sure to remove the product');
         if(proceed){
-            console.log('Deleted',id);
+            const url=`http://localhost:5000/product/${id}`;
+            fetch(url,{
+                method:'DELETE',
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                const remaining=products.filter(product=>product._id!==id)
+                setProducts(remaining)
+            })
         }
     }
 
@@ -17,7 +26,7 @@ const ManageProducts = () => {
            <h2 className='text-center text-warning'>Manage your products!</h2> 
 
            {
-               products.map(product=><div className='container card shadow p-3 my-5 mx-auto w-50' key={product._id}>
+               products.map(product=><div className='container card shadow p-3 my-5 mx-auto w-50 text-center' key={product._id}>
                 <img style={{height:'200px'}} src={product.picture} alt="" />
                 <h4>{product.name}</h4>
                 <h5>Price:{product.price}</h5>
