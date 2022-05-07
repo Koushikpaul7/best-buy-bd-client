78bf73fdb309f7cfe635a74ead93bd5dce4898a7
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-05-07 14:27:04 +0600

    my items added

--- src/Components/AddProducts/AddProducts.js
+++ src/Components/AddProducts/AddProducts.js
@@ -1,10 +1,16 @@
-import React from 'react';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from "react-hook-form";
+import auth from '../../firebase.init';
 
 const AddProducts = () => {
+    const [user]=useAuthState(auth)
+
+
     const { register, handleSubmit } = useForm();
     const onSubmit = data => {
         console.log(data);
+
         const url=`http://localhost:5000/product`;
         fetch(url,{
             method:'POST',
@@ -23,13 +29,15 @@ const AddProducts = () => {
         <div className='w-50 mx-auto mb-5'>
             <h2 className='text-center mt-4 '>Add new product for Best Buy Bd</h2>
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+
       <input placeholder='Name' className='mb-2 rounded' {...register("name", { required: true, maxLength: 20 })} />
+      <input placeholder='Name' className='mb-2 rounded' value={user.email} {...register("email", { required: true, maxLength: 200 })} />
       <input placeholder='price' className='mb-2 rounded' {...register("price", { required: true, maxLength: 20 })} />
       <input placeholder='photo url' className='mb-2 rounded' {...register("picture", { required: true, maxLength: 1000 })} />
       <input placeholder='company' className='mb-2 rounded' {...register("company", { required: true, maxLength: 20 })} />
       <input placeholder='description' className='mb-2 rounded' {...register("description")} />
       <input placeholder='quantity' className='mb-2 rounded' type="number" {...register("quantity")} />
-      <input type="submit" value='Add product' />
+      <input className='btn btn-success rounded shadow' type="submit" value='Add product' />
     </form>
         </div>
     );
--- src/Components/AddProducts/MyProducts.js
+++ src/Components/AddProducts/MyProducts.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import './MyProduct.css'
 
 const MyProducts = () => {
+    const[myProducts, setMyProducts]=useState([]);
+    const [user]=useAuthState(auth);
+    useEffect(()=>{
+    const getMyProducts=async()=>{
+        const email=user.email;
+        const url=`http://localhost:5000/myproducts?email=${email}`;
+        const {data}=await axios.get(url,{
+            headers:{
+                authorization:`Bearer ${localStorage.getItem('accessToken')}`
+            }
+        });
+           
+        setMyProducts(data);
+    }
+    getMyProducts();
+},[user])
     return (
-        <div>
-            Added Products By user:
+        <div className='text-center container mb-5'>
+          <h3 className='mt-4'>  Added Products By  {user.email} is:
+            {myProducts.length}</h3>
+            <div className='mt-5 mb-5'>
+            <table>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Brand</th>
+          </tr>
+                {
+                    myProducts.map(pd=>
+                        <tr key={pd._id}>
+                        <td>{pd.name}</td>
+                        <td>{pd.price}</td>
+                        <td>{pd.quantity}</td>
+                        <td>{pd.company}</td>
+                        </tr>)
+                }
+                </table>
+            </div>
         </div>
     );
 };
